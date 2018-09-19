d15d7874df4cdb16746b9116f44f4ed236ac2704
Hsatou Exceedone <hsatou@exceedone.co.jp>
2018-09-19 11:11:00 +0900

    bug fix template installer

--- resources/views/dashboard/system/guideline.blade.php
+++ resources/views/dashboard/system/guideline.blade.php
@@ -3,5 +3,5 @@
     <br />現在、随時機能追加中です！
 </p>
 <p>・サイト(株式会社エクシード・ワン​ 公式サイト内)：<a href="https://www.exceedone.co.jp/exment/" target="_blank">こちら</a></p>
-<p>・GitHub：準備中</p>
+<p>・GitHub：<a href="https://github.com/exceedone/exment" target="_blank">こちら</a></p>
 <p>・Document：<a href="https://exment.net/docs/#/" target="_blank">こちら</a></p>
