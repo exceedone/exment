3a06e9dee572ceb0e0a91848dca08e2ec8554780
Hsatou Exceedone <hsatou@exceedone.co.jp>
2018-12-27 18:33:15 +0900

    bug fix

--- src/Middleware/Morph.php
+++ src/Middleware/Morph.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Exceedone\Exment\Model;
-use Exceedone\Exment\Model\CustomTable;
 use Exceedone\Exment\Model\Define;
 use Exceedone\Exment\Enums\SystemTableName;
 
@@ -25,24 +24,27 @@ class Morph
     public static function defineMorphMap()
     {
         // morphMap
-        if (Schema::hasTable(CustomTable::getTableName())) {
-            $table_names = CustomTable::all(['table_name'])->pluck('table_name');
-            $morphMaps = [
+        try {
+            if (Schema::hasTable(SystemTableName::CUSTOM_TABLE)) {
+                $table_names = CustomTable::all(['table_name'])->pluck('table_name');
+                $morphMaps = [
                 "roles" => Model\Role::class,
                 "table" => CustomTable::class
             ];
-            foreach ($table_names as $table_name) {
-                // morphmap
-                $morphMaps[$table_name] = ltrim(getModelName($table_name, true), "\\");
-            }
-            Relation::morphMap($morphMaps);
+                foreach ($table_names as $table_name) {
+                    // morphmap
+                    $morphMaps[$table_name] = ltrim(getModelName($table_name, true), "\\");
+                }
+                Relation::morphMap($morphMaps);
 
-            // Define Modelname user and org.
-            $tables = [SystemTableName::USER, SystemTableName::ORGANIZATION];
-            //$tables = CustomTable::all();
-            foreach ($tables as $table) {
-                getModelName($table);
+                // Define Modelname user and org.
+                $tables = [SystemTableName::USER, SystemTableName::ORGANIZATION];
+                //$tables = CustomTable::all();
+                foreach ($tables as $table) {
+                    getModelName($table);
+                }
             }
-        }
+        }catch(\Exception $ex)
+        {}
     }
 }
