7f5f4ca320c88dd512069f2b146f8db392d13ad5
Hsatou Exceedone <hsatou@exceedone.co.jp>
2019-03-20 09:34:04 +0900

    remove Document Pdf Service. (Not use function. mistake)

--- src/Services/DocumentExcelService.php
+++ src/Services/DocumentExcelService.php
@@ -1,11 +1,5 @@
 <?php
 /*
- * This file is part of the Order Pdf plugin
- *
- * Copyright (C) 2016 LOCKON CO.,LTD. All Rights Reserved.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 
 namespace Exceedone\Exment\Services;
