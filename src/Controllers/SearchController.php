1b2227ed0b3d52f87e319e05e6a12019f7d836c2
Hsatou Exceedone <hsatou@exceedone.co.jp>
2019-04-15 18:52:02 +0900

    Bug fix search controller check permission

--- src/Controllers/SearchController.php
+++ src/Controllers/SearchController.php
@@ -516,7 +516,7 @@ EOT;
         if (isset($search_type)) {
             $array['search_type'] = $search_type;
         }
-        if ($table->hasPermission(Permission::AVAILABLE_VIEW_CUSTOM_VALUE)) {
+        if (CustomTable::getEloquent($table)->hasPermission(Permission::AVAILABLE_VIEW_CUSTOM_VALUE)) {
             $array['show_list'] = true;
         }
         return $array;
