2cdcc534b5abd1aad4a2afd8ad1de962414dd3c7
Hsatou Exceedone <hsatou@exceedone.co.jp>
2019-04-15 11:55:44 +0900

    merge migtration file to v1_1

--- database/migrations/2019_04_15_115120_support_for_v1_1.php
+++ database/migrations/2019_04_15_115120_support_for_v1_1.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
+use Exceedone\Exment\Database\ExtendedBlueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
 use Exceeone\Exment\Model\CustomColumn;
 use Exceeone\Exment\Enums\CurrencySymbol;
 
-class AlterCustomViewColumns extends Migration
+class SupportForV11 extends Migration
 {
     /**
      * Run the migrations.
@@ -16,6 +17,30 @@ class AlterCustomViewColumns extends Migration
      */
     public function up()
     {
+        $schema = DB::connection()->getSchemaBuilder();
+
+        $schema->blueprintResolver(function($table, $callback) {
+            return new ExtendedBlueprint($table, $callback);
+        });
+
+        $schema->create('custom_view_summaries', function (ExtendedBlueprint $table) {
+            $table->increments('id');
+            $table->integer('custom_view_id')->unsigned();
+            $table->integer('view_column_type')->default(0);
+            $table->integer('view_column_target_id')->nullable();
+            $table->integer('view_summary_condition')->unsigned()->default(0);
+            $table->string('view_column_name', 40)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->timeusers();
+
+            $table->foreign('custom_view_id')->references('id')->on('custom_views');
+        });
+
+        $schema->table('custom_view_columns', function($table) {
+            $table->string('view_column_name', 40)->nullable();
+        });
+
         Schema::table('custom_view_columns', function (Blueprint $table) {
             $table->integer('view_column_table_id')->after('view_column_type')->unsigned();
         });
@@ -122,5 +147,12 @@ class AlterCustomViewColumns extends Migration
         Schema::table('custom_columns', function (Blueprint $table) {
             $table->dropColumn('order');
         });
+        
+        Schema::dropIfExists('custom_view_summaries');
+
+        Schema::table('custom_view_columns', function($table) {
+            $table->dropColumn('view_column_name');
+        });
+        
     }
 }
