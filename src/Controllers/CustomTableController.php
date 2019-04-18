24697f7428c55f90a2415e82f012c51602ef66e0
yisaka117 <yisaka@exceedone.co.jp>
2019-04-18 19:01:53 +0900

    bug list id : 42

--- src/Controllers/CustomTableController.php
+++ src/Controllers/CustomTableController.php
@@ -35,8 +35,10 @@ class CustomTableController extends AdminControllerBase
     protected function grid()
     {
         $grid = new Grid(new CustomTable);
+        //$grid->setResource('/admin/table/setorder');
         $grid->column('table_name', exmtrans("custom_table.table_name"))->sortable();
         $grid->column('table_view_name', exmtrans("custom_table.table_view_name"))->sortable();
+        $grid->column('order', exmtrans("custom_table.order"))->editable()->sortable();
         
         $grid->tools(function (Grid\Tools $tools) {
             $tools->append(new Tools\GridChangePageMenu('table', null, true));
@@ -80,6 +82,9 @@ class CustomTableController extends AdminControllerBase
             ->required()
             ->help(exmtrans('common.help.view_name'));
         $form->textarea('description', exmtrans("custom_table.field_description"))->rows(3);
+        
+        
+        $form->number('order', exmtrans("custom_table.order"));
         
         $form->header(exmtrans('common.detail_setting'))->hr();
         $form->embeds('options', exmtrans("custom_column.options.header"), function ($form) use ($id) {
