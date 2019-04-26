d45631466eabb2817d5979c5fc5b2101fe7e6288
hirossyi <hirossyi@hirossyinoMacBook-Pro.local>
2019-04-26 16:25:51 +0900

    modify relationship function path

--- src/Model/CustomViewColumn.php
+++ src/Model/CustomViewColumn.php
@@ -40,24 +40,6 @@ class CustomViewColumn extends ModelBase
         ],
     ];
 
-    public function custom_view()
-    {
-        return $this->belongsTo(CustomView::class, 'custom_view_id');
-    }
-    
-    public function custom_column()
-    {
-        if ($this->view_column_type == ViewColumnType::SYSTEM) {
-            return null;
-        }
-        return $this->belongsTo(CustomColumn::class, 'view_column_target_id');
-    }
-    
-    public function custom_table()
-    {
-        return $this->belongsTo(CustomTable::class, 'view_column_table_id');
-    }
-
     /**
      * get eloquent using request settion.
      * now only support only id.
