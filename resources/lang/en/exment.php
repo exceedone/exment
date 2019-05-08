98afb0e2cca5f5068fe2a750a0d605b6683cff33
hirossyi73 <it2@hirossyi.net>
2019-05-08 16:40:56 +0900

    add link to backup automatic

--- resources/lang/en/exment.php
+++ resources/lang/en/exment.php
@@ -279,7 +279,7 @@ return [
             'file_name' => 'Please select the backup zip file.',
             'import_error_message' => 'If incomplete files are incomplete, an error message is displayed in this item.',
             'backup_target' => 'The target of backup. Unless there is a particular reason, we recommend checking all items.',
-            'enable_automatic' => 'If it is set to YES, backup will be executed automatically.',
+            'enable_automatic' => 'If it is set to YES, backup will be executed automatically.<br /><b>*You have to do scheduling setting.</b> Please refer to <a href="%s" target="_blank">here<i class="fa fa-external-link"></i></a>.',
             'automatic_term' => 'It is a setting of how many days to perform automatic backup. Example: When entering "3", execute backup every 3 days',
             'automatic_hour' => 'The start time of the automatic backup. Example: When entering "3", start backup execution at 3:00',
             'history_files' => 'The number of generations to keep during automatic backup. When 1 or more is input, the backup of the input integer is kept.',
