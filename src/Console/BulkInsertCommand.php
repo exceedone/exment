707cee806cedcb187684661e1b7370ffe11cb69f
hirossyi73 <it2@hirossyi.net>
2019-05-09 09:47:17 +0900

    each file's execute bulkinsert

--- src/Console/BulkInsertCommand.php
+++ src/Console/BulkInsertCommand.php
@@ -47,11 +47,20 @@ class BulkInsertCommand extends Command
             // get target directory (argument)
             $dir = $this->argument("dir");
 
-            // convert all csv files in target folder
-            $path = $this->convertFiles($dir);
+            // get all csv file names in target directory
+            $files = $this->getFiles($dir, 'csv');
 
-            // import tsv file to database table
-            $this->importTsv($path);
+            $this->line("該当ファイル数：".count($files));
+
+            foreach($files as $index => $file){
+                $this->line(($index + 1) . "件目 実施開始 ファイル:{$file->getFileName()}");
+
+                // convert all csv files in target folder
+                $path = $this->convertFile($dir, $file);
+
+                // import tsv file to database table
+                $this->importTsv($path);
+            }
 
             // delete working folder
             File::deleteDirectory($path);
@@ -86,16 +95,13 @@ class BulkInsertCommand extends Command
      * convert all csv files in target folder into tsv files
      *
      */
-    private function convertFiles($dir, $include_sub = false)
+    private function convertFile($dir, $file, $include_sub = false)
     {
 
         // check if directory is exists
         if (!File::isDirectory($dir)) {
             throw new \Exception('Not found directory : ' . $dir);
         }
-
-        // get all csv file names in target directory
-        $files = $this->getFiles($dir, 'csv', $include_sub);
 
         // get temp directory full path
         $tempdir = storage_path('app/tmp/bulkins/' . date('YmdHis'));
@@ -106,72 +112,73 @@ class BulkInsertCommand extends Command
         }
 
         // convert csv file to tsv file
-        $this->convertTsv($tempdir, $files);
+        $outputpath = $this->convertTsv($tempdir, $file);
 
-        return $tempdir;
+        return $outputpath;
     }
 
     /**
      * convert csv files to tsv files
      *
      */
-    private function convertTsv($tempdir, $files)
+    private function convertTsv($tempdir, $file)
     {
-        foreach ($files as $file) {
-            // 1:table_name, (2:sequence number) , 3:extension
-            $targets = explode('.', $file->getFilename());
-            // get physical table name
-            $tablename = getDBTableName($targets[0]);
+        // 1:table_name, (2:sequence number) , 3:extension
+        $targets = explode('.', $file->getFilename());
+        // get physical table name
+        $tablename = getDBTableName($targets[0]);
 
-            switch (count($targets)) {
-                case 2:
-                    $tsvname = $tablename . '.tsv';
-                    break;
-                case 3:
-                    // if file has sequence, format number for sort
-                    $tsvname = $tablename . '.' . str_pad($targets[1], 10, '0', STR_PAD_LEFT) . '.tsv';
-                    break;
-                default:
-                    $this->warn('invalid file name (skipped) : ' . $file->getFilename());
-                    break;
-            }
-            // get table all column names
-            $columns = Schema::getColumnListing($tablename);
-
-            // open input file as csv
-            $fileobj = $file->openFile();
-            $fileobj->setFlags(
-                \SplFileObject::READ_CSV |
-                \SplFileObject::READ_AHEAD |
-                \SplFileObject::SKIP_EMPTY
-            );
-
-            // get locale
-            $locales = explode('.', setlocale(LC_CTYPE, '0'));
-            // set locale UTF-8 (if sjis)
-            if (count($locales) >= 2 && $locales[1] == '932') {
-                setlocale(LC_CTYPE, $locales[0] . '.UTF-8');
-            }
-            // create output file as tsv
-            $output = new \SplFileObject(path_join($tempdir, $tsvname), 'w');
-            $output->setCsvControl("\t");
-
-            $i = 0;
-            $header = [];
-            foreach ($fileobj as $line) {
-                if ($i == 0) {
-                    // store header data
-                    $header = $line;
-                    // write all column names
-                    $records = $columns;
-                } else {
-                    $records = $this->getTsvLine($columns, $header, $line);
-                }
-
-                $output->fputcsv($records);
-                $i++;
-            }
+        switch (count($targets)) {
+            case 2:
+                $tsvname = $tablename . '.tsv';
+                break;
+            case 3:
+                // if file has sequence, format number for sort
+                $tsvname = $tablename . '.' . str_pad($targets[1], 10, '0', STR_PAD_LEFT) . '.tsv';
+                break;
+            default:
+                $this->warn('invalid file name (skipped) : ' . $file->getFilename());
+                break;
         }
+        // get table all column names
+        $columns = Schema::getColumnListing($tablename);
+
+        // open input file as csv
+        $fileobj = $file->openFile();
+        $fileobj->setFlags(
+            \SplFileObject::READ_CSV |
+            \SplFileObject::READ_AHEAD |
+            \SplFileObject::SKIP_EMPTY
+        );
+
+        // get locale
+        $locales = explode('.', setlocale(LC_CTYPE, '0'));
+        // set locale UTF-8 (if sjis)
+        if (count($locales) >= 2 && $locales[1] == '932') {
+            setlocale(LC_CTYPE, $locales[0] . '.UTF-8');
+        }
+        // create output file as tsv
+        $outputpath = path_join($tempdir, $tsvname);
+        $output = new \SplFileObject($outputpath, 'w');
+        $output->setCsvControl("\t");
+
+        $i = 0;
+        $header = [];
+        foreach ($fileobj as $line) {
+            if ($i == 0) {
+                // store header data
+                $header = $line;
+                // write all column names
+                $records = $columns;
+            } else {
+                $records = $this->getTsvLine($columns, $header, $line);
+            }
+
+            $output->fputcsv($records);
+            $i++;
+        }
+
+        return $outputpath;
     }
 
     /**
@@ -203,7 +210,7 @@ class BulkInsertCommand extends Command
         // extract header item that matches the field name
         $keys = preg_grep('/^'.$field.'(\..+)?$/i', $header);
         if (count($keys) == 0) {
-            return '';
+            return null;
         }
         $ary = [];
         foreach ($keys as $key => $value) {
@@ -221,32 +228,31 @@ class BulkInsertCommand extends Command
     /**
      * insert table data from tsv files.
      *
-     * @param string temporary folder path stored work tsv files
+     * @param string temporary file path stored work tsv files
      */
-    private function importTsv($dir)
+    private function importTsv($file)
     {
+        $file = new \SplFileInfo($file);
         // load table data from tsv file
-        foreach ($this->getFiles($dir) as $file) {
-            $targets = explode('.', $file->getFilename());
-            $cmd =<<<__EOT__
-            LOAD DATA local INFILE '%s' 
-            INTO TABLE %s 
-            CHARACTER SET 'UTF8' 
-            FIELDS TERMINATED BY '\t' 
-            OPTIONALLY ENCLOSED BY '\"' 
-            ESCAPED BY '\"' 
-            LINES TERMINATED BY '\\n' 
-            IGNORE 1 LINES 
-            SET created_at = NOW(),
-                updated_at = NOW(),
-                deleted_at = nullif(deleted_at, '0000-00-00 00:00:00'),
-                created_user_id = nullif(created_user_id, 0),
-                updated_user_id = nullif(updated_user_id, 0),
-                deleted_user_id = nullif(deleted_user_id, 0),
-                parent_id = nullif(parent_id, 0)
+        $targets = explode('.', $file->getFileName());
+        $cmd =<<<__EOT__
+        LOAD DATA local INFILE '%s' 
+        INTO TABLE %s 
+        CHARACTER SET 'UTF8' 
+        FIELDS TERMINATED BY '\t' 
+        OPTIONALLY ENCLOSED BY '\"' 
+        ESCAPED BY '\"' 
+        LINES TERMINATED BY '\\n' 
+        IGNORE 1 LINES 
+        SET created_at = NOW(),
+            updated_at = NOW(),
+            deleted_at = nullif(deleted_at, '0000-00-00 00:00:00'),
+            created_user_id = nullif(created_user_id, 0),
+            updated_user_id = nullif(updated_user_id, 0),
+            deleted_user_id = nullif(deleted_user_id, 0),
+            parent_id = nullif(parent_id, 0)
 __EOT__;
-            $query = sprintf($cmd, addslashes($file->getPathName()), $targets[0]);
-            $cnt = \DB::connection()->getpdo()->exec($query);
-        }
+        $query = sprintf($cmd, addslashes($file->getPathName()), $targets[0]);
+        $cnt = \DB::connection()->getpdo()->exec($query);
     }
 }
