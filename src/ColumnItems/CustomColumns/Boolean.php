4ff72dbc00449c95c90edb48f3e026b8ab6f744d
hirossyi73 <it2@hirossyi.net>
2019-05-15 13:44:40 +0900

    Bug fix Switch FIeld

--- src/ColumnItems/CustomColumns/Boolean.php
+++ src/ColumnItems/CustomColumns/Boolean.php
@@ -3,7 +3,7 @@
 namespace Exceedone\Exment\ColumnItems\CustomColumns;
 
 use Exceedone\Exment\ColumnItems\CustomItem;
-use Encore\Admin\Form\Field;
+use Exceedone\Exment\Form\Field;
 use Encore\Admin\Grid\Filter;
 
 class Boolean extends CustomItem
