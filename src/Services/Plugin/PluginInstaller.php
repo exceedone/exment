a5a2fe15bdafc2ca71b63b5b2e2beffbb0ca153a
hirossyi73 <it2@hirossyi.net>
2019-05-21 14:47:15 +0900

    Bug fix cannot showing pluginlist

--- src/Services/Plugin/PluginInstaller.php
+++ src/Services/Plugin/PluginInstaller.php
@@ -280,7 +280,7 @@ class PluginInstaller
         // execute query
         return Plugin::where('active_flg', '=', 1)
             ->whereIn('plugin_type', [PluginType::TRIGGER, PluginType::DOCUMENT])
-            ->where('options->target_tables', $table_name_escape)
+            ->whereJsonContains('options->target_tables', $table_name)
             ->get()
             ;
     }
