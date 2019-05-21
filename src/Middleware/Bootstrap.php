bd3f5ee901336cd08ae14c005007c70f20cc1166
yisaka117 <yisaka@exceedone.co.jp>
2019-05-21 10:00:33 +0900

    add weekly, dayly option to calendar view

--- src/Middleware/Bootstrap.php
+++ src/Middleware/Bootstrap.php
@@ -33,6 +33,7 @@ class Bootstrap
         Ad::css(asset('vendor/exment/fullcalendar/core/main.min.css?ver='.$ver));
         Ad::css(asset('vendor/exment/fullcalendar/daygrid/main.min.css?ver='.$ver));
         Ad::css(asset('vendor/exment/fullcalendar/list/main.min.css?ver='.$ver));
+        Ad::css(asset('vendor/exment/fullcalendar/timegrid/main.min.css?ver='.$ver));
         Ad::js(asset('vendor/exment/js/common.js?ver='.$ver));
         //Ad::js(asset('vendor/exment/js/common.js'));
         Ad::js(asset('vendor/exment/chartjs/chart.min.js'));
@@ -42,6 +43,7 @@ class Bootstrap
         Ad::js(asset('vendor/exment/fullcalendar/interaction/main.min.js?ver='.$ver));
         Ad::js(asset('vendor/exment/fullcalendar/daygrid/main.min.js?ver='.$ver));
         Ad::js(asset('vendor/exment/fullcalendar/list/main.min.js?ver='.$ver));
+        Ad::js(asset('vendor/exment/fullcalendar/timegrid/main.min.js?ver='.$ver));
         
         // add admin_url and file delete confirm
         $delete_confirm = trans('admin.delete_confirm');
