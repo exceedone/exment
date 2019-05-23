5fe51be74d4065a01f31d58b7b0b3af9bc934156
hirossyi73 <it2@hirossyi.net>
2019-05-23 17:14:01 +0900

    Bug fix if user access user in modal

---
+++ database/migrations/2019_05_23_165335_patch_user_accessable.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Exceedone\Exment\Model\CustomTable;
+use Exceedone\Exment\Enums\SystemTableName;
+
+class PatchUserAccessable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // update user and autority accessable
+        foreach([SystemTableName::USER, SystemTableName::ORGANIZATION] as $tableName){
+            $table = CustomTable::getEloquent($tableName);
+            if(!isset($table)){
+                continue;
+            }
+
+            $table->setOption('all_user_accessable_flg', "1");
+            $table->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
