dbbe52c73e772fd10a1a84833b712236f8df10c4
hirossyi73 <it2@hirossyi.net>
2019-05-28 22:52:46 +0900

    add config in listitem

--- src/DashboardBoxItems/ListItem.php
+++ src/DashboardBoxItems/ListItem.php
@@ -139,6 +139,7 @@ class ListItem implements ItemInterface
         $form->select('pager_count', trans("admin.show"))
             ->required()
             ->options(getPagerOptions(false, [5, 10, 20]))
+            ->config('allowClear', false)
             ->default(5);
 
         $form->select('target_table_id', exmtrans("dashboard.dashboard_box_options.target_table_id"))
