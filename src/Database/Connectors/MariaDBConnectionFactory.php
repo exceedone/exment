b24b22503cf1092a70e3846720b3468556d5f3ef
hirossyi73 <it2@hirossyi.net>
2019-05-31 23:52:02 +0900

    Support MariaDB

---
+++ src/Database/Connectors/MariaDBConnectionFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Exceedone\Exment\Database\Connectors;
+
+use Illuminate\Database\Connectors\MySqlConnector;
+use Exceedone\Exment\Database\MariaDBConnection;
+
+class MariaDBConnectionFactory extends \Illuminate\Database\Connectors\ConnectionFactory
+{ 
+    public function createConnector(array $config)
+    {
+        return new MySqlConnector;
+    }
+
+    protected function createConnection($driver, $connection, $database, $prefix = '', array $config = [])
+    {
+        return new MariaDBConnection($connection, $database, $prefix, $config);
+    }
+}
