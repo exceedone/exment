cd4723d38d253cb2abff0f7f5acc570fd41295db
hirossyi73 <it2@hirossyi.net>
2019-07-04 07:16:05 +0900

    modify before release

--- resources/views/custom-value/new-button.blade.php
+++ resources/views/custom-value/new-button.blade.php
@@ -1,9 +1,5 @@
 <div class="btn-group pull-right" style="margin-right: 5px">
-@if(isset($params))
-    <a href="{{admin_url('data/'.$table_name.'/create'). '?' . http_build_query($params)}}" class="btn btn-sm btn-success">
-@else
-    <a href="{{admin_url('data/'.$table_name.'/create')}}" class="btn btn-sm btn-success">
-@endif
+    <a href="{{admin_url('data/'.$table_name.'/create'). ( isset($params) ? '?' . http_build_query($params) : '' ) }}" class="btn btn-sm btn-success">
         <i class="fa fa-plus"></i><span class="hidden-xs">&nbsp;&nbsp;{{trans('admin.new')}}</span>
     </a>
 </div>
