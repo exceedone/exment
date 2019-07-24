d7d26f464908d9f741c3761be04f0ac9439e720a
hirossyi73 <it2@hirossyi.net>
2019-07-25 00:32:23 +0900

    remove unused items

--- src/Model/NotifyNavbar.php
+++ src/Model/NotifyNavbar.php
@@ -11,21 +11,5 @@ class NotifyNavbar extends ModelBase
             return $builder->where('target_user_id', \Exment::user()->base_user_id)
                 ->orderBy('read_flg', 'asc')->orderBy('created_at', 'desc');
         });
-
-        // delete overflow rows after created.
-        // static::created(function ($model) {
-        //     $count = self::withoutGlobalScope('target_user')
-        //                 ->where('target_user_id', $model->target_user_id)
-        //                 ->count();
-        //     $max_count = config('exment.notify_page_max', 100);
-
-        //     if ($count > $max_count) {
-        //         // delete overflow rows
-        //         self::withoutGlobalScope('target_user')
-        //             ->orderBy('read_flg', 'desc')
-        //             ->orderBy('created_at', 'asc')
-        //             ->take($count - $max_count)->delete();
-        //     }
-        // });
     }
 }
