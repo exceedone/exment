209ba6241a0694b64f627c43b00d1024f1be0ece
hirossyi73 <it2@hirossyi.net>
2019-07-27 18:38:06 +0900

    forget remove allrecords in customview

--- src/Model/CustomView.php
+++ src/Model/CustomView.php
@@ -847,48 +847,4 @@ class CustomView extends ModelBase implements Interfaces\TemplateImporterInterfa
     {
         return boolval($this->view_kind_type == ViewKindType::ALLDATA);
     }
-    
-    /**
-     * get all records. use system session
-     */
-    public static function allRecords(\Closure $filter = null, $isGetAll = true)
-    {
-        $key = sprintf(Define::SYSTEM_KEY_SESSION_ALL_RECORDS, self::getTableName());
-        // get from request session
-        $records = System::requestSession($key, function () {
-            return self::all();
-        });
-
-        // execute filter
-        if (isset($filter)) {
-            $records = $records->filter(function ($record) use ($filter) {
-                return $filter($record);
-            });
-        }
-
-        // if exists, return
-        if (count($records) > 0) {
-            return $records;
-        }
-        
-        if ((!isset($records) || count($records) == 0) && !$isGetAll) {
-            return $records;
-        }
-
-        // else, get all again
-        $records = self::all();
-        System::requestSession($key, $records);
-
-        if (!isset($records)) {
-            return $records;
-        }
-
-        // execute filter
-        if (isset($filter)) {
-            $records = $records->filter(function ($record) use ($filter) {
-                return $filter($record);
-            });
-        }
-        return $records;
-    }
 }
