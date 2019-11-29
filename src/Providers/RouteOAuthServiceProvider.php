9e15dfbddef4fd5c9ce567568288c0dff47e7c5b
hirossyi73 <it2@hirossyi.net>
2019-11-29 18:51:29 +0900

    Bug fix adminapi_anonymous

--- src/Providers/RouteOAuthServiceProvider.php
+++ src/Providers/RouteOAuthServiceProvider.php
@@ -151,7 +151,7 @@ class RouteOAuthServiceProvider extends ServiceProvider
         return [
             'prefix' => url_join(config('admin.route.prefix'), 'oauth'),
             'namespace' => '\Laravel\Passport\Http\Controllers',
-            'middleware' => ['adminapi'],
+            'middleware' => ['adminapi_anonymous'],
         ];
     }
     protected function getOauthWebOptions()
