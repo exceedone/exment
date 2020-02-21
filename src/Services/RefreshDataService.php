69e8a2adc1485a656a21e551f6c8837e3082a59d
hirossyi73 <it2@hirossyi.net>
2020-02-21 20:44:42 +0900

    Append removeing attachment

--- src/Services/RefreshDataService.php
+++ src/Services/RefreshDataService.php
@@ -6,6 +6,7 @@ use Exceedone\Exment\Model\CustomRelation;
 use Exceedone\Exment\Model\Notify;
 use Exceedone\Exment\Model\NotifyTarget;
 use Exceedone\Exment\Model\NotifyNavbar;
+use Exceedone\Exment\Model\Define;
 use Exceedone\Exment\Enums\RelationType;
 use Exceedone\Exment\Enums\SystemTableName;
 use Exceedone\Exment\Model\File as ExmentFile;
@@ -59,14 +60,15 @@ class RefreshDataService
         , $tables);
 
         // exm__ tables (ignore org)
-        $tables = array_merge(CustomTable
+        $custom_tables = CustomTable
             ::whereNotIn('id', [$user->id, $org->id, $mail_template->id])
             ->get()
-            ->map(function($table){
-                return getDBTableName($table);
-            })
             ->filter(function($table){
-                return hasTable($table);
+                return hasTable(getDBTableName($table));
+            });
+
+        $tables = array_merge($custom_tables->map(function($table){
+                return getDBTableName($table);
             })->toArray()
         , $tables);
 
@@ -76,5 +78,34 @@ class RefreshDataService
                 \DB::table($table)->truncate();
             }
         });
+
+        // remove attachment files
+        static::removeAttachmentFiles($custom_tables);
+    }
+
+    /**
+     * Remove attachment files
+     *
+     * @param [type] $custom_tables
+     * @return void
+     */
+    public static function removeAttachmentFiles($custom_tables){
+        $disk = \Storage::disk(Define::DISKNAME_ADMIN);
+
+        foreach($custom_tables as $custom_table){
+            // remove file table
+            ExmentFile::where('parent_type', $custom_table->table_name)
+                ->delete();
+
+            if (!$disk->exists($custom_table->table_name)) {
+                continue;
+            }
+
+            // if avatar or system, continue
+            if(in_array($custom_table->table_name, ['avatar', 'system'])){
+                continue;
+            }
+            deleteDirectory($disk, $custom_table->table_name);
+        }
     }
 }
