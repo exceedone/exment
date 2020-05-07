dcae86f5444b0c2a3af9da24a0cec22904c3c3c2
hiroshi.satou <https://kajitori@dev.azure.com/kajitori/SharePointCSOM/_git/SharePointCSOM>
2020-05-07 16:02:27 +0900

    Cannot publish for develop packages

--- src/Console/InstallUpdateTrait.php
+++ src/Console/InstallUpdateTrait.php
@@ -11,6 +11,23 @@ trait InstallUpdateTrait
     protected $directory = '';
 
     /**
+     * Publish static files
+     *
+     * @return void
+     */
+    public function publishStaticFiles()
+    {
+        $this->call('vendor:publish', ['--provider' => \Exceedone\Exment\ExmentServiceProvider::class, '--tag' => 'laravel-admin-lang-exment', '--force' => true]);
+        $this->call('vendor:publish', ['--provider' => \Exceedone\Exment\ExmentServiceProvider::class, '--tag' => 'laravel-admin-assets-exment', '--force' => true]);
+        $this->call('vendor:publish', ['--provider' => \Exceedone\Exment\ExmentServiceProvider::class, '--tag' => 'public', '--force' => true]);
+        $this->call('vendor:publish', ['--provider' => \Exceedone\Exment\ExmentServiceProvider::class, '--tag' => 'lang', '--force' => true]);
+        $this->call('vendor:publish', ['--provider' => \Exceedone\Exment\ExmentServiceProvider::class, '--tag' => 'views_vendor', '--force' => true]);
+        
+        // not force
+        $this->call('vendor:publish', ['--provider' => \Exceedone\Exment\ExmentServiceProvider::class, '--tag' => 'lang_vendor']);
+    }
+    
+    /**
      * Create routes file.
      *
      * @return void
