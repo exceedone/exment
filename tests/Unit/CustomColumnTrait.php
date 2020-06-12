38ae698ae59de7d6095ebfd4b6e3838441abb649
hirossyi73 <it2@hirossyi.net>
2020-06-13 00:10:08 +0900

    Append custom column and value test

---
+++ tests/Unit/CustomColumnTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Exceedone\Exment\Tests\Unit;
+
+use Exceedone\Exment\Model\CustomTable;
+use Exceedone\Exment\Model\CustomColumn;
+use Exceedone\Exment\Model\CustomValue;
+use Exceedone\Exment\Enums\ColumnType;
+use Exceedone\Exment\Enums\ValueType;
+use Exceedone\Exment\Enums\CurrencySymbol;
+use Exceedone\Exment\Enums\SystemTableName;
+
+trait CustomColumnTrait
+{
+    /**
+     * Get CustomColumn model.
+     * *We can create custom column model if not save database.
+     *
+     * @return CustomColumn
+     */
+    protected function getCustomColumnModel($column_type, $options = []){
+        $custom_column = new CustomColumn([
+            'column_name' => $column_type,
+            'column_view_name' => $column_type,
+            'column_type' => $column_type,
+            'options' => $options,
+        ]);
+
+        return $custom_column;
+    }
+
+    /**
+     * Get Custom Value and column item.
+     * *We can create custom value model if not save database.
+     *
+     * @return CustomColumn
+     */
+    protected function getCustomValueAndColumnItem(&$custom_column, $value){
+        $classname = getModelName(CustomTable::getEloquent('information'));
+        $custom_value = new $classname([
+            'id' => 1, // dummy
+            'value' => [
+                $custom_column->column_name => $value,
+            ],
+        ]);
+        
+        $column_item = $custom_column->column_item->setCustomValue($custom_value);
+
+        return [$custom_value, $column_item];
+    }
+}
