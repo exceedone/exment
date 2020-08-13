f707ad2c5036f7c8daaee4c76b05c61628ad6815
yisaka117 <yisaka@exceedone.co.jp>
2020-08-13 11:00:10 +0900

    custom table cannot delete when it has user view

--- src/Model/CustomTable.php
+++ src/Model/CustomTable.php
@@ -398,7 +398,7 @@ class CustomTable extends ModelBase implements Interfaces\TemplateImporterInterf
         foreach ($this->custom_forms as $item) {
             $item->deletingChildren();
         }
-        foreach ($this->custom_views as $item) {
+        foreach ($this->custom_views()->withoutGlobalScope('showableViews')->get() as $item) {
             $item->deletingChildren();
         }
         foreach ($this->from_custom_copies as $item) {
