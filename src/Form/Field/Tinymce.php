0391e6f88a8c5afb2ded4b6a86cded237f2f3217
hirossyi73 <it2@hirossyi.net>
2020-09-18 17:33:56 +0900

    Modify file upload script logic

--- src/Form/Field/Tinymce.php
+++ src/Form/Field/Tinymce.php
@@ -85,32 +85,7 @@ class Tinymce extends Textarea
             $configs = array_merge([
                 'automatic_uploads' => true,
                 'file_picker_types' => 'image',
-                'file_picker_callback' => "function (cb, value, meta) {
-                    var input = document.createElement('input');
-                    input.setAttribute('type', 'file');
-                    input.setAttribute('accept', 'image/*');
-                
-                    input.onchange = function () {
-                    var file = this.files[0];
-                
-                    var reader = new FileReader();
-                    reader.onload = function () {
-                        var id = 'blobid' + (new Date()).getTime();
-                        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-                        var base64 = reader.result.split(',')[1];
-                        var blobInfo = blobCache.create(id, file, base64);
-                        blobCache.add(blobInfo);
-                
-                        /* call the callback and populate the Title field with the file name */
-                        cb(blobInfo.blobUri(), { title: file.name });
-                    };
-                    reader.readAsDataURL(file);
-                    };
-                
-                    input.click();
-                },
-                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-                })",
+                'content_style' => 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
             ], $configs);
         }
 
@@ -166,6 +141,31 @@ class Tinymce extends Textarea
                 formData.append('file', blobInfo.blob(), blobInfo.filename());
             
                 xhr.send(formData);
+            };
+
+            config['file_picker_callback'] = function (cb, value, meta) {
+                var input = document.createElement('input');
+                input.setAttribute('type', 'file');
+                input.setAttribute('accept', 'image/*');
+            
+                input.onchange = function () {
+                    var file = this.files[0];
+                
+                    var reader = new FileReader();
+                    reader.onload = function () {
+                        var id = 'blobid' + (new Date()).getTime();
+                        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                        var base64 = reader.result.split(',')[1];
+                        var blobInfo = blobCache.create(id, file, base64);
+                        blobCache.add(blobInfo);
+                
+                        /* call the callback and populate the Title field with the file name */
+                        cb(blobInfo.blobUri(), { title: file.name });
+                    };
+                    reader.readAsDataURL(file);
+                };
+            
+                input.click();
             };
         }
 
