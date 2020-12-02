5769f7f58121a3377ccbc65cbdc9dbacd1834629
yisaka117 <yisaka@exceedone.co.jp>
2020-12-02 11:45:04 +0900

    temporary commit

--- tests/Unit/ImportExportTest.php
+++ tests/Unit/ImportExportTest.php
@@ -23,7 +23,7 @@ class ImportExportTest extends UnitTestBase
     protected function init(bool $export, $target_name = null)
     {
         $this->initAllTest();
-        $this->be(LoginUser::find(TestDefine::TESTDATA_USER_LOGINID_USER1));
+        $this->be(LoginUser::find(TestDefine::TESTDATA_USER_LOGINID_ADMIN));
         if ($export) {
             $this->dirpath = storage_path(path_join_os('app', 'export', 'unittest'));
             if (\File::exists($this->dirpath)) {
@@ -119,9 +119,62 @@ class ImportExportTest extends UnitTestBase
         ]);
     }
 
+    public function testExportCsvWithSetting()
+    {
+        $table_name = 'custom_value_edit_all';
+
+        $custom_view = CustomView::where('view_view_name', "$table_name-view-odd")->first();
+
+        $this->_testExport([
+            'table_name' => $table_name,
+            '--action' => 'view',
+            '--view' => $custom_view,
+            '--add_setting' => 1,
+        ]);
+    }
+
+    public function testExportXlsxWithRelation()
+    {
+        $table_name = 'parent_table';
+
+        $this->_testExport([
+            'table_name' => $table_name,
+            '--format' => 'xlsx',
+            '--add_relation' => 1,
+        ]);
+    }
+
+    public function testChunkExportDefault()
+    {
+        $table_name = 'custom_value_edit_all';
+
+        $this->_testChunkExport([
+            'table_name' => $table_name,
+        ], [1]);
+    }
+
+    public function testChunkExportRange()
+    {
+        $table_name = 'custom_value_edit_all';
+
+        $this->_testChunkExport([
+            'table_name' => $table_name,
+            '--start' => 2,
+            '--end' => 4,
+            '--count' => 20,
+            '--seqlength' => 3,
+            '--format' => 'xlsx',
+        ], ['002', '003', '004']);
+    }
+
     public function testImport()
     {
         $this->_testImport('import_test_1');
+    }
+
+    public function testImportMulti()
+    {
+        $this->_testImport('import_test_2');
     }
 
     protected function _testExport(array $params)
@@ -143,6 +196,29 @@ class ImportExportTest extends UnitTestBase
         $file_path = path_join($params['--dirpath'], $params['table_name'] . '.' . $params['--format']);
         $this->assertTrue(\File::exists($file_path));
         $this->assertTrue(\File::size($file_path) > 0);
+    }
+
+    protected function _testChunkExport(array $params, array $numRange)
+    {
+        $this->init(true);
+
+        $params = array_merge(
+            [
+                '--dirpath' => $this->dirpath,
+                '--format' => 'csv',
+            ], 
+            $params
+        );
+
+        $result = \Artisan::call('exment:chunkexport', $params);
+
+        $this->assertEquals($result, 0);
+
+        foreach ($numRange as $num) {
+            $file_path = path_join($params['--dirpath'], $params['table_name'] . ".$num." . $params['--format']);
+            $this->assertTrue(\File::exists($file_path));
+            $this->assertTrue(\File::size($file_path) > 0);
+        }
     }
 
     protected function _testImport($target_name)
