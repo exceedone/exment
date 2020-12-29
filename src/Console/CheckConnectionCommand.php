1c954281b7bc01a7697aa924dc2e86cf4fff0f6f
hsatou <it2@hirossyi.net>
2020-12-29 23:32:15 +0900

    Append check connection

---
+++ src/Console/CheckConnectionCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Exceedone\Exment\Console;
+
+use Encore\Admin\Console\InstallCommand as AdminInstallCommand;
+
+class CheckConnectionCommand extends AdminInstallCommand
+{
+    use CommandTrait;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'exment:check-connection';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check connection database, if connect, return 1.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->initExmentCommand();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if(\ExmentDB::canConnection()){
+            $this->line('Connected');
+            return 1;
+        }
+        $this->error('Not Connected');
+        return 0;
+    }
+}
--- src/ExmentServiceProvider.php
+++ src/ExmentServiceProvider.php
@@ -77,6 +77,7 @@ class ExmentServiceProvider extends ServiceProvider
         'Exceedone\Exment\Console\ExportCommand',
         'Exceedone\Exment\Console\ExportChunkCommand',
         'Exceedone\Exment\Console\ResetPasswordCommand',
+        'Exceedone\Exment\Console\CheckConnectionCommand',
     ];
 
     
