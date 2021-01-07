e6aff4ee87c88b3429f6d34f3864dbf82baa28d6
yisaka117 <yisaka@exceedone.co.jp>
2021-01-07 16:47:40 +0900

    update for laravel 6X unittest setup result etc

--- src/Model/Define.php
+++ src/Model/Define.php
@@ -143,7 +143,7 @@ class Define
         'format_local',
     ];
 
-    public const CACHE_CLEAR_MINUTE = 60;
+    public const CACHE_CLEAR_MINUTE = 3600;
     public const SYSTEM_KEY_SESSION_SYSTEM_CONFIG = "setting.%s";
     public const SYSTEM_KEY_SESSION_INITIALIZE = "initialize";
     public const SYSTEM_KEY_SESSION_INITIALIZE_INPUTS = "initialize_inputs";
