10bca145756064b3af2cfbe3f1f0e14c78aea754
hsatou <it2@hirossyi.net>
2021-01-11 19:36:44 +0900

    wrong package name

--- src/Model/Define.php
+++ src/Model/Define.php
@@ -14,8 +14,9 @@ use Exceedone\Exment\Enums\Permission;
  */
 class Define
 {
-    public const COMPOSER_PACKAGE_NAME = 'kajitoritest/laravel-admin';
-    public const COMPOSER_PACKAGE_NAME_LARAVEL_ADMIN = 'kajitoritest/exment';
+    //TODO: Please revert if exceedone/exment
+    public const COMPOSER_PACKAGE_NAME = 'kajitoritest/exment';
+    public const COMPOSER_PACKAGE_NAME_LARAVEL_ADMIN = 'kajitoritest/laravel-admin';
     public const COMPOSER_VERSION_CHECK_URL = 'https://repo.packagist.org/p/exceedone/exment.json';
     public const EXMENT_NEWS_API_URL = 'https://exment.net/wp-json/wp/v2/posts';
     public const EXMENT_NEWS_LINK = 'https://exment.net/archives/category/news';
