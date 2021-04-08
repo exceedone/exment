b2ac97f74997b3c4491f1fc08bcd7a4198ad8654
yisaka117 <yisaka@exceedone.co.jp>
2021-04-08 16:22:02 +0900

    remove hidden from viewonly field

--- src/Form/Field/ViewOnly.php
+++ src/Form/Field/ViewOnly.php
@@ -8,30 +8,4 @@ namespace Exceedone\Exment\Form\Field;
  */
 class ViewOnly extends Display
 {
-    protected $view = 'exment::form.field.view_only';
-
-    protected $prepareDefault = false;
-
-    public function prepareDefault()
-    {
-        $this->prepareDefault = true;
-        return $this;
-    }
-
-    public function prepare($value)
-    {
-        // Even if set value, return always default.
-        return $this->default;
-    }
-    
-    /**
-     * {@inheritdoc}
-     */
-    public function render()
-    {
-        return parent::render()->with([
-            'prepareDefault'   => $this->prepareDefault,
-            'default'          => $this->default,
-        ]);
-    }
 }
