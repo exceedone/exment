ebec424588fd76d4f4578fb710d70e05f8a1bfc8
hsatou <it2@hirossyi.net>
2022-02-24 14:35:30 +0900

    remove unused $config->set 775

--- src/Services/Helpers.php
+++ src/Services/Helpers.php
@@ -99,7 +99,6 @@ if (!function_exists('html_clean')) {
         try {
             // default setting for exment
             $config = HTMLPurifier_Config::createDefault();
-            $config->set('Cache.SerializerPermissions', 0775);
             $config->set('HTML.Allowed', Define::HTML_ALLOWED_DEFAULT);
             $config->set('HTML.AllowedAttributes', Define::HTML_ALLOWED_ATTRIBUTES_DEFAULT);
             $config->set('CSS.AllowedProperties', Define::CSS_ALLOWED_PROPERTIES_DEFAULT);
