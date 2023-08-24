ac926f47035a7279b0c93915a24cae5684e82877
ex1anhth <anhth@trithucmoi.co>
2023-08-24 17:11:47 +0700

    Issue#1185 fix plugin button not show in calendar view

--- src/Facades/ExmentFacade.php
+++ src/Facades/ExmentFacade.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Facade;
  */
 class ExmentFacade extends Facade
 {
+    // @phpstan-ignore-next-line
     protected static function getFacadeAccessor()
     {
         return \Exceedone\Exment\Exment::class;
