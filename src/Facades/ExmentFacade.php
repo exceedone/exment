39af18f0a724cf93b6c4eef57a2e4346aa2b9d97
Nobuyuki Tabuchi <n.tabuchi@rolo.jp>
2023-08-29 00:16:55 +0900

    fix phpstan

--- src/Facades/ExmentFacade.php
+++ src/Facades/ExmentFacade.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Facade;
  */
 class ExmentFacade extends Facade
 {
-    // @phpstan-ignore-next-line
     protected static function getFacadeAccessor()
     {
         return \Exceedone\Exment\Exment::class;
