1818076f48f63820415a6c51b2d5b9d13fc01aa0
vohoangnhat <vohoangnhat@gmail.com>
2024-01-15 14:49:41 +0700

    Support laravel >= 10.34.00

--- src/Database/ConnectionTrait.php
+++ src/Database/ConnectionTrait.php
@@ -94,7 +94,7 @@ trait ConnectionTrait
 
             // If we catch an exception we'll rollback this transaction and try again if we
             // are not out of attempts. If we are out of attempts we will just throw the
-            // exception back out and let the developer handle an uncaught exceptions.
+            // exception back out, and let the developer handle an uncaught exception.
             catch (Throwable $e) {
                 $this->handleTransactionException(
                     $e,
@@ -113,14 +113,20 @@ trait ConnectionTrait
                 }
 
                 if ($this->transactions == 1) {
+                    $this->fireConnectionEvent('committing');
                     $this->getPdo()->commit();
                 }
 
-                $this->transactions = max(0, $this->transactions - 1);
+                [$levelBeingCommitted, $this->transactions] = [
+                    $this->transactions,
+                    max(0, $this->transactions - 1),
+                ];
 
-                if ($this->transactions == 0) {
-                    optional($this->transactionsManager)->commit($this->getName());
-                }
+                $this->transactionsManager?->commit(
+                    $this->getName(),
+                    $levelBeingCommitted,
+                    $this->transactions
+                );
             } catch (Throwable $e) {
                 $this->handleCommitTransactionException(
                     $e,
