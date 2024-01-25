21b8a18e34bc6c6f94349aa2efe6c08dd006ca43
yisaka117 <yisaka@exceedone.co.jp>
2024-01-25 17:59:51 +0900

    revert file.php

--- src/ColumnItems/CustomColumns/File.php
+++ src/ColumnItems/CustomColumns/File.php
@@ -35,17 +35,6 @@ class File extends CustomItem
     }
 
     /**
-     * get file name
-     */
-    public function file_name()
-    {
-        $file = ExmentFile::getData($this->fileValue($this->value));
-        if ($file) {
-            return $file->filename;
-        }
-    }
-
-    /**
      * get text
      */
     protected function _text($v)
