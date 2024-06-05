b5b6e4eee1b6fe0890dde9c39154a369661aee40
anhth <anhth@trithucmoi.co>
2024-06-05 11:34:51 +0700

    Issue #1450 Fix warning PHP Deprecated when install package

--- tests/Browser/CCustomViewTest.php
+++ tests/Browser/CCustomViewTest.php
@@ -214,7 +214,7 @@ class CCustomViewTest extends ExmentKitTestCase
                     $callback($query, $v);
                 }
 
-                $this->assertTrue($query->exists(), "custom view items not contains items.　custom_view_id:${id}, k:${k}, key:${key}, view_target:{$v['view_column_target']}, view_column_type:${column_type}, view_column_target_id:{$column_type_target}, view_column_table_id:${column_table_id}");
+                $this->assertTrue($query->exists(), "custom view items not contains items.　custom_view_id:{$id}, k:{$k}, key:{$key}, view_target:{$v['view_column_target']}, view_column_type:{$column_type}, view_column_target_id:{$column_type_target}, view_column_table_id:{$column_table_id}");
             }
         }
     }
