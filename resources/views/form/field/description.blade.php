d033a2edba4231a77005cf0e28a5734fd893e495
SuDC <sudc@trithucmoi.co>
2025-02-06 13:42:04 +0700

    wip

--- resources/views/form/field/description.blade.php
+++ resources/views/form/field/description.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    <div class="{{$viewClass['field']}} {{$offset}}" {!! $attributes !!} >
+    <div class="{{$viewClass['field']}} {{$offset}} ms-auto p-3 me-5" {!! $attributes !!} >
         @if($escape)
         {{ $label }}
         @else
--- resources/views/tools/modal-button.blade.php
+++ resources/views/tools/modal-button.blade.php
@@ -1,5 +1,5 @@
 <a id="{{$suuid}}" href="javascript:void(0);" data-widgetmodal_url="{{$url}}" 
-    class="btn btn-sm {{$button_class}} pull-right" style="margin-right:5px;" title="{{$label}}">
+    class="btn {{$button_class}} pull-right d-flex align-items-center text-white" style="margin-right:5px;" title="{{$label}}">
     <i class="fa {{$icon}}"></i>
     &nbsp;<span class="hidden-xs">{{$label}}</span>
 </a>
