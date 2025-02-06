dfb6884dcd7a0f31c4d0f1e731dc20ccfcfb48c7
ex1vanla <vanla@trithucmoi.co>
2025-02-06 16:39:47 +0900

    issue: button

--- resources/views/tools/modal-button.blade.php
+++ resources/views/tools/modal-button.blade.php
@@ -1,5 +1,5 @@
 <a id="{{$suuid}}" href="javascript:void(0);" data-widgetmodal_url="{{$url}}" 
-    class="btn btn-sm {{$button_class}} pull-right" style="margin-right:5px;" title="{{$label}}">
+    class="btn btn-sm {{$button_class}} pull-right justify-content-center align-items-center" style="margin-right:5px;" title="{{$label}}">
     <i class="fa {{$icon}}"></i>
     &nbsp;<span class="hidden-xs">{{$label}}</span>
 </a>
