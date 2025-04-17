5bec6cc6aca3ead571d88d48794be65cd23472a7
ex1vanla <vanla@trithucmoi.co>
2025-04-17 13:57:04 +0900

    fix data version screen

--- resources/views/custom-value/revision-compare.blade.php
+++ resources/views/custom-value/revision-compare.blade.php
@@ -1,21 +1,21 @@
-<div class="box box-revision-compare">
+<div class="box box-revision-compare custom-border-secondary card p-2">
     <div class="box-header with-border">
-        <h3 class="box-title">
+        <h3 class="box-title float-start p-1 px-3">
             {{exmtrans('revision.revision')}}
         </h3>
         @if($has_edit_permission)
-        <div class="btn-group pull-right" style="margin-right: 5px">
+        <div class="btn-group float-end" style="margin-right: 5px">
             <a href="{{$custom_value->getUrl(['uri' => 'edit'])}}" class="btn btn-sm btn-primary" title="{{ trans('admin.edit') }}">
                 <i class="fa fa-edit"></i><span class="d-none d-md-inline"> {{ trans('admin.edit') }}</span>
             </a>
         </div>
         @endif
-        <div class="btn-group pull-right" style="margin-right: 5px">
+        <div class="btn-group float-end" style="margin-right: 5px">
             <a href="{{$show_url}}" class="btn btn-sm btn-default" title="{{ trans('admin.show') }}">
                 <i class="fa fa-eye"></i><span class="d-none d-md-inline"> {{ trans('admin.show') }}</span>
             </a>
         </div>
-        <div class="btn-group pull-right" style="margin-right: 5px">
+        <div class="btn-group float-end" style="margin-right: 5px">
             <a href="{{$custom_value->getUrl(['list' => true])}}" class="btn btn-sm btn-default" title="{{ trans('admin.list') }}">
                 <i class="fa fa-list"></i><span class="d-none d-md-inline"> {{ trans('admin.list') }}</span>
             </a>
@@ -26,8 +26,8 @@
         <div class="form-horizontal">
             <div class="box-body">
                 <div class="fields-group">
-                    <div class="form-group" style="margin-bottom:2em;">
-                        <label class="col-sm-2 control-label">{{exmtrans('revision.revision_select')}}</label>
+                    <div class="form-group row pt-4" style="margin-bottom:2em;">
+                        <label class="col-sm-2 col-form-label">{{exmtrans('revision.revision_select')}}</label>
                         <div class="col-sm-5">
                             <select id="revisions" data-add-select2 data-add-select2-allow-clear="false">
                                 @foreach($revisions as $index => $revision)
@@ -47,10 +47,9 @@
                     
                     <div class="row">
                         <div class="col-sm-12">
-                            <h4 class="field-header">{{exmtrans('revision.compare_revision')}}</h4>
+                            <h4 class="field-header box-header with-border pb-2" style="margin: 0px 70px;">{{exmtrans('revision.compare_revision')}}</h4>
                         </div>
                     </div>
-                    <hr style="margin-top: 0px;" />
                     
                     <div id="pjax-container-revision">
                         @include('exment::custom-value.revision-compare-inner') 
