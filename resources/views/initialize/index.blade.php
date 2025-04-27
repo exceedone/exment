05ede096fa1ef7314e60a42e55ec94789c821d79
ex1vanla <vanla@trithucmoi.co>
2025-04-27 19:22:10 +0900

    fix initialize screen

--- resources/views/initialize/index.blade.php
+++ resources/views/initialize/index.blade.php
@@ -2,17 +2,16 @@
 <html lang="{{ config('app.locale') }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ Admin::title() }}</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/open-admin/AdminLTE/bootstrap/css/bootstrap.min.css") }}">
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ admin_asset("/vendor/open-admin/font-awesome/css/font-awesome.min.css") }}">
 
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/open-admin/AdminLTE/dist/css/skins/" . config('admin.skin') .".min.css") }}">
+    <!-- AdminLTE Theme style (BS5-compatible) -->
+    <link rel="stylesheet" href="{{ admin_asset("/vendor/open-admin/AdminLTE/dist/css/adminlte.min.css") }}">
 
     {!! Admin::css() !!}
     <link rel="stylesheet" href="{{ admin_asset("/vendor/open-admin/open-admin/open-admin.css") }}">
@@ -22,35 +21,26 @@
     <link rel="stylesheet" href="{{ admin_asset("/vendor/open-admin/toastr/build/toastr.min.css") }}">
     <link rel="stylesheet" href="{{ admin_asset("/vendor/open-admin/bootstrap3-editable/css/bootstrap-editable.css") }}">
     <link rel="stylesheet" href="{{ admin_asset("/vendor/open-admin/google-fonts/fonts.css") }}">
-    <link rel="stylesheet" href="{{ admin_asset("/vendor/open-admin/AdminLTE/dist/css/AdminLTE.min.css") }}">
 
     <!-- REQUIRED JS SCRIPTS -->
     <script src="{{ admin_asset ("/vendor/open-admin/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js") }}"></script>
-    <script src="{{ admin_asset ("/vendor/open-admin/AdminLTE/bootstrap/js/bootstrap.min.js") }}"></script>
+    <!-- Bootstrap 5 JS (No jQuery dependency) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="{{ admin_asset ("/vendor/open-admin/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js") }}"></script>
-    <script src="{{ admin_asset ("/vendor/open-admin/AdminLTE/dist/js/app.min.js") }}"></script>
+    <script src="{{ admin_asset ("/vendor/open-admin/AdminLTE/dist/js/adminlte.min.js") }}"></script>
     <script src="{{ admin_asset ("/vendor/open-admin/jquery-pjax/jquery.pjax.js") }}"></script>
     <script src="{{ admin_asset ("/vendor/open-admin/nprogress/nprogress.js") }}"></script>
-
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 </head>
 
 <body class="hold-transition {{config('admin.skin')}} {{join(' ', config('admin.layout'))}}">
 <div class="wrapper">
-
-    <div class="" id="pjax-container" style="width:80%; min-width:960px; margin:0 auto;">
+    <div class="container-fluid" id="pjax-container" style="width:80%; min-width:960px; margin:0 auto;">
         @yield('content')
         {!! Admin::script() !!}
     </div>
-
 </div>
 
-<!-- ./wrapper -->
-
+<!-- CSRF Token for Laravel -->
 <script>
     function LA() {}
     LA.token = "{{ csrf_token() }}";
@@ -59,12 +49,12 @@
 @if (session('status'))
 <script>
     $(function () {
-      toastr.success('{{ session("status") }}', null, []);
-  });
+        toastr.success('{{ session("status") }}', null, []);
+    });
 </script>
 @endif
 
-<!-- REQUIRED JS SCRIPTS -->
+<!-- Additional JS Scripts -->
 <script src="{{ admin_asset ("/vendor/open-admin/nestable/jquery.nestable.js") }}"></script>
 <script src="{{ admin_asset ("/vendor/open-admin/toastr/build/toastr.min.js") }}"></script>
 <script src="{{ admin_asset ("/vendor/open-admin/bootstrap3-editable/js/bootstrap-editable.min.js") }}"></script>
