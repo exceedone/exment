780ba8e65e2c1d5b67c21343e2d392cb2194016b
yisaka117 <yisaka@exceedone.co.jp>
2025-05-20 09:06:02 +0900

    rollback and change comment filter logic

--- src/ColumnItems/CommentItem.php
+++ src/ColumnItems/CommentItem.php
@@ -2,16 +2,12 @@
 
 namespace Exceedone\Exment\ColumnItems;
 
-use Encore\Admin\Form\Field\Text;
 use Exceedone\Exment\Enums\SystemColumn;
-use Exceedone\Exment\Enums\FilterOption;
-use Exceedone\Exment\Model\Define;
-use Exceedone\Exment\Model\System;
+use Exceedone\Exment\Enums\SystemTableName;
+use Exceedone\Exment\Model\CustomColumn;
 
 class CommentItem extends SystemItem
 {
-    protected $table_name = 'comment_values';
-
     /**
      * constructor
      */
@@ -21,26 +17,7 @@ class CommentItem extends SystemItem
         $this->setCustomValue($custom_value);
 
         $this->column_name = SystemColumn::COMMENT;
-        $this->label = exmtrans('common.format_keyvalue', 
-            exmtrans("common.$this->column_name"), 
-            exmtrans('system.filter_search_type_options.all'));
-    }
-
-    /**
-     * whether column is enabled index.
-     *
-     */
-    public function sortable()
-    {
-        return false;
-    }
-
-    /**
-     * get sql query column name
-     */
-    protected function getSqlColumnName(bool $appendTable)
-    {
-        return 'comment';
+        $this->label = exmtrans("common.$this->column_name");
     }
 
     public static function getItem(...$args)
@@ -50,71 +27,24 @@ class CommentItem extends SystemItem
     }
 
     /**
-     * get text(for display)
-     */
-    protected function _text($v)
-    {
-        return null;
-    }
-
-    /**
-     * get html(for display)
-     * *this function calls from non-escaping value method. So please escape if not necessary unescape.
-     */
-    protected function _html($v)
-    {
-        return null;
-    }
-
-    public function getFilterField($value_type = null)
-    {
-        $field = new Text($this->name(), [$this->label()]);
-        $field->default($this->value);
-        return $field;
-    }
-
-    /**
-     * get
-     */
-    public function getTableName()
-    {
-        return $this->table_name;
-    }
-
-
-    /**
-     * get real table name.
-     * If workflow, this name is workflow view.
-     */
-    public function sqlRealTableName()
-    {
-        return $this->getTableName();
-    }
-
-    /**
-     * Get grid filter option. Use grid filter, Ex. LIKE search.
+     * Set where query for grid filter. 
      *
-     * @return string|null
-     */
-    protected function getGridFilterOption(): ?string
-    {
-        return (string)FilterOption::LIKE;
-    }
-
-    /**
-     * Set admin filter options
-     *
-     * @param $filter
+     * @param \Illuminate\Database\Query\Builder|\Illuminate\Database\Schema\Builder|\Illuminate\Database\Eloquent\Builder $query
+     * @param mixed $input
      * @return void
-     * @throws \Psr\Container\ContainerExceptionInterface
-     * @throws \Psr\Container\NotFoundExceptionInterface
      */
-    protected function setAdminFilterOptions(&$filter)
+    public function getAdminFilterWhereQuery($query, $input)
     {
-        // Whether executed search.
-        $searched = boolval(request()->get($filter->getId()));
-        if ($searched) {
-            System::setRequestSession(Define::SYSTEM_KEY_SESSION_COMMENT_FILTER_CHECK, true);
-        }
+        $tableName = getDBTableName($this->custom_table);
+        $tableNameComment = getDBTableName(SystemTableName::COMMENT);
+        $columnName = CustomColumn::getEloquent('comment_detail', SystemTableName::COMMENT)->getQueryKey();
+
+        $query->whereExists(function ($subQuery) use ($input, $tableName, $tableNameComment, $columnName) {
+            $subQuery->select(\DB::raw(1))
+                ->from($tableNameComment)
+                ->whereColumn("{$tableNameComment}.parent_id", "{$tableName}.id")
+                ->where("{$tableNameComment}.parent_type", $this->custom_table->table_name)
+                ->where("{$tableNameComment}.{$columnName}", 'LIKE', "%{$input}%");
+        });
     }
 }
