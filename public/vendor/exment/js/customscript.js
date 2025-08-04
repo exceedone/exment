0878dd0f2464c758a34c2f0443e714a1427871f2
SuDC <sudc@trithucmoi.co>
2025-08-04 15:02:26 +0700

    revert code

--- public/vendor/exment/js/customscript.js
+++ public/vendor/exment/js/customscript.js
@@ -5,7 +5,10 @@ var Exment;
     const EVENT_FORM_LOADED = 'exment:form_loaded';
     const EVENT_LIST_LOADED = 'exment:list_loaded';
     const EVENT_SHOW_LOADED = 'exment:show_loaded';
-
+    //const EVENT_CALENDAR_BIND = 'exment:calendar_bind'; // Used by calendar.blade.php
+    /**
+    * Column Event Script.
+    */
     class CustomScriptEvent {
         static AddEvent() {
             CustomScriptEvent.fireEvent();
@@ -13,94 +16,37 @@ var Exment;
             CustomScriptEvent.fireFormEvent();
             CustomScriptEvent.fireShowEvent();
         }
-
         static AddEventOnce() {
             $(document).on('pjax:complete', function (event) {
                 CustomScriptEvent.AddEvent();
             });
             $(window).trigger(EVENT_FIRST_LOADED);
         }
-
         static fireEvent() {
             $(window).trigger(EVENT_LOADED);
         }
-
         static fireFormEvent() {
-            if (!$('.block_custom_value_form').length) {
+            if (!hasValue($('.block_custom_value_form'))) {
                 return;
             }
             $(window).trigger(EVENT_FORM_LOADED);
         }
-
         static fireListEvent() {
-            if (!$('.block_custom_value_grid').length) {
+            if (!hasValue($('.block_custom_value_grid'))) {
                 return;
             }
             $(window).trigger(EVENT_LIST_LOADED);
         }
-
         static fireShowEvent() {
-            if (!$('.block_custom_value_show').length) {
+            if (!hasValue($('.block_custom_value_show'))) {
                 return;
             }
             $(window).trigger(EVENT_SHOW_LOADED);
         }
-
-        static setLoading(event) {
-            event.preventDefault();
-
-            const button = event.target;
-            if (!(button && (button.classList.contains('submit') || button.type === 'submit'))) {
-                return;
-            }
-
-            const originalText = button.innerHTML;
-            const originalDisabledState = button.disabled;
-            button.innerHTML = 'Loading...';
-            button.disabled = true;
-
-            const form = $(button).closest('form');
-
-            if (form.data('submitted')) {
-                button.innerHTML = originalText;
-                button.disabled = originalDisabledState;
-                return;
-            }
-
-            form.data('submitted', true);
-
-            setTimeout(() => {
-                button.innerHTML = originalText;
-                button.disabled = originalDisabledState;
-                form.data('submitted', false);
-            }, 2000);
-
-            if (form.length) {
-                form.submit();
-            }
-        }
-
-        static bindSubmitButtons() {
-            $(document).on('click', 'button.submit, button[type="submit"], #search-btn', function (event) {
-                Exment.CustomScriptEvent.setLoading(event);
-            });
-        }
     }
-
     Exment.CustomScriptEvent = CustomScriptEvent;
 })(Exment || (Exment = {}));
-
-// jQuery ready
 $(function () {
-    Exment.CustomScriptEvent.bindSubmitButtons();
     Exment.CustomScriptEvent.AddEvent();
     Exment.CustomScriptEvent.AddEventOnce();
-
-    $(document).on('pjax:complete', function () {
-        Exment.CustomScriptEvent.bindSubmitButtons();
-    });
-
-    $('#filter-box').on('show', function () {
-        Exment.CustomScriptEvent.bindSubmitButtons();
-    });
 });
