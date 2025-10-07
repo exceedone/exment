9e15f23ee441bd176900207c9db631f3d408b161
ex1anhth+quangdv <anhth@trithucmoi.co>
2025-10-07 16:38:16 +0700

    css ui button

--- resources/views/tools/modal-menu-button.blade.php
+++ resources/views/tools/modal-menu-button.blade.php
@@ -1,7 +1,7 @@
 <div class="btn-group float-end p-0">
     @if((!is_null($ajax) && trim($ajax) !== '') || !empty($menulist) || (!empty($icon) && !empty($label)))
     <a class="btn justify-content-center align-items-center d-flex  p-2 {{$button_class}} {{!is_nullorempty($menulist) ? 'dropdown-toggle' : ''}}" 
-        style="margin-right:5px;"
+        style="margin-right:5px; font-size:12px !important;"
         data-bs-toggle="dropdown"
         data-widgetmodal_url="{{$ajax ?? ''}}"
         data-widgetmodal_method="GET"
