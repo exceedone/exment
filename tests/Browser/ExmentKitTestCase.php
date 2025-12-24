2e2d1a670428e85126de1fbde5f79acc4e4a7351
manhhh1108 <duongmanh2106@gmail.com>
2025-12-24 17:20:11 +0700

    fix: ensure locale is set correctly in ExmentKitTestCase setup

--- tests/Browser/ExmentKitTestCase.php
+++ tests/Browser/ExmentKitTestCase.php
@@ -31,10 +31,12 @@ abstract class ExmentKitTestCase extends BaseTestCase
     {
         // cannot call method "config", so call env function
         $this->baseUrl = env('APP_URL');
+        putenv('APP_LOCALE=ja');
         parent::setUp();
         System::clearCache();
+        config(['app.locale' => 'ja']);
         config(['exment.locale' => 'ja']);
-        \App::setLocale('ja');
+        \App::setLocale('ja');;
     }
 
     /**
