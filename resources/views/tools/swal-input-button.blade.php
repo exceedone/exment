4cde963357627af406120d9bea445c222bcbb448
anhth+quangdv <anhth@trithucmoi.co>
2026-06-18 15:47:23 +0900

    fix: update button styling and improve import statement in TableService

--- resources/views/tools/swal-input-button.blade.php
+++ resources/views/tools/swal-input-button.blade.php
@@ -1,5 +1,5 @@
 <div class="btn-group pull-right" style="margin-right: 5px">
     <a href="javascript:void(0);" class="btn btn-sm d-flex justify-content-center align-items-center {{ $btn_class ?? 'btn-default' }} btn-{{$suuid}}" title="{{ $label }}">
-        <i class="fa {{ $icon }}"></i><span class="d-none d-md-inline">&nbsp;{{ $label }}</span>
+        <i class="fa {{ $icon }} p-1"></i><span class="d-none d-md-inline">&nbsp;{{ $label }}</span>
     </a>
 </div>
