8bf97454deef8e72df0c7a3caffcbdb69bfeff4f
SuDC <sudc@trithucmoi.co>
2026-07-21 17:15:23 +0700

    fix(security):deletion post-initialization (JVN#20312919)

--- src/Controllers/TemplateController.php
+++ src/Controllers/TemplateController.php
@@ -297,6 +297,13 @@ class TemplateController extends AdminControllerBase
     // @phpstan-ignore-next-line
     public function delete(Request $request)
     {
+        if (System::initialized()) {
+            $login_user = \Exment::user();
+            if (!$login_user || !$login_user->hasPermission(Permission::SYSTEM)) {
+                abort(403);
+            }
+        }
+
         // install templates selected tiles.
         if ($request->has('template')) {
             $importer = new TemplateImportExport\TemplateImporter();
